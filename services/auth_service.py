5c11c62328d4023bddfdb5ab15d7842eac962753
taijarals <taijara@outlook.com.br>
2026-04-20 18:02:34 -0300

    auth_service

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,12 +1,41 @@
-from repositories.usuario_repo import get_user_by_email
+# services/auth_service.py
+
+from supabase import create_client
+from config.settings import settings
+
+supabase = create_client(
+    settings.SUPABASE_URL,
+    settings.SUPABASE_KEY
+)
 
 def login_user(email, senha):
-    user = get_user_by_email(email)
+    response = supabase.auth.sign_in_with_password({
+        "email": email,
+        "password": senha
+    })
 
-    if not user:
+    if response.user:
+        return response.user
+    
+    return None
+
+
+def register_user(email, senha, nick):
+    # 1. cria no auth
+    response = supabase.auth.sign_up({
+        "email": email,
+        "password": senha
+    })
+
+    if not response.user:
         return None
 
-    if user["senha"] != senha:
-        return None
+    user_id = response.user.id
 
-    return user
+    # 2. cria perfil
+    supabase.table("usuario2").insert({
+        "id_usuario": user_id,
+        "nick_usuario": nick
+    }).execute()
+
+    return response.user
