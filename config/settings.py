087b56fce91ccaa5d7c19998a52810331768750c
taijarals <taijara@outlook.com.br>
2026-04-20 18:40:48 -0300

    .replace("/rest/v1", "")

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 class Settings:
-    SUPABASE_URL = st.secrets["SUPABASE_URL"]
+    SUPABASE_URL = st.secrets["SUPABASE_URL"].replace("/rest/v1", "")
     SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 settings = Settings()
