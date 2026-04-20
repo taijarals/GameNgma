ac5d777430f140eac5813f435f76208456bf7203
taijarals <taijara@outlook.com.br>
2026-04-20 18:02:57 -0300

    login

--- pages/login.py
+++ pages/login.py
@@ -1,18 +1,37 @@
 import streamlit as st
-from services.auth_service import login_user
+from services.auth_service import login_user, register_user
 from utils.session import set_user
 
 def render():
-    st.title("Login")
+    st.title("Acesso")
 
-    email = st.text_input("Email")
-    senha = st.text_input("Senha", type="password")
+    aba = st.radio("Escolha uma opção", ["Login", "Cadastro"])
 
-    if st.button("Entrar"):
-        user = login_user(email, senha)
+    if aba == "Login":
+        email = st.text_input("Email")
+        senha = st.text_input("Senha", type="password")
 
-        if user:
-            set_user(user)
-            st.rerun()  # recarrega e vai pro main
-        else:
-            st.error("Credenciais inválidas")
+        if st.button("Entrar"):
+            user = login_user(email, senha)
+
+            if user:
+                set_user(user)
+                st.success("Login realizado!")
+                st.rerun()
+            else:
+                st.error("Email ou senha inválidos")
+
+    else:  # Cadastro
+        email = st.text_input("Email", key="cad_email")
+        senha = st.text_input("Senha", type="password", key="cad_senha")
+        nick = st.text_input("Nick", key="cad_nick")
+
+        if st.button("Cadastrar"):
+            user = register_user(email, senha, nick)
+
+            if user:
+                set_user(user)
+                st.success("Cadastro realizado!")
+                st.rerun()
+            else:
+                st.error("Erro ao cadastrar usuário")
